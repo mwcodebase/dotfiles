743e9fe4a643fa6270aeb4a028f831a94fb4bd46
mwcodebase <mwaldron.personal@gmail.com>
2020-02-26 22:57:08 -0500

    refactoring repo

--- Windows/powershell_setup.ps1
+++
@@ -1,5 +0,0 @@
-# Change the two user-name fields to what is appropriate... \Users\{here} and \home\{here}
-
-Set-ExecutionPolicy RemoteSigned
-Set-Location C:\Users\Michael\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\rootfs\home\mwaldron\zsh_env\fonts\
-.\install.ps1
