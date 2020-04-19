2ac77866ae73adf9e142d5e17b84263356b9f1d9
mwcodebase <mwaldron.personal@gmail.com>
2020-04-19 13:00:08 -0400

    removed packages and user dir setup

--- scripts/home_setup.sh
+++ scripts/home_setup.sh
@@ -1,32 +1,14 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-   printf "This script must be run as root" 
+if [[ ! $EUID -ne 0 ]]; then
+   printf "Do not run this script as root" 
    exit 1
 fi
 
+set -e
 cd $HOME
 
-# install any packages that have yet to be installed
-
-apt-get update -yq
-apt-get upgrade -yq
-
-INSTALLED_PACKAGES="$(dpkg --get-selections | grep -v deinstall)"
-NEW_PACKAGES="vim gnome-tweaks python3 python3-pip python3-tk curl default-jdk vim ca-certificates zsh dos2unix"
-
-for i in $NEW_PACKAGES; do
-  if [[ ! "$INSTALLED_PACKAGES" == *"$i"* ]]; then
-    apt-get install -yq $i
-  fi
-done
-
-if [[ ! "$INSTALLED_PACKAGES" == *"fonts-powerline"* ]]; then 
-  apt-get install -yq powerline fonts-powerline
-fi
-
 # setup symlinks so dotfiles can be managed by git (if not already set)
-
 if [ -f $HOME/.aliases ]; then
   if [ ! -L $HOME/.aliases ]; then
     printf "Pre-existing .aliases file found. Please backup and remove it so the symlink can be created.\n"
@@ -51,28 +33,7 @@ else
   ln -sv $HOME/dotfiles/sink/.vimrc $HOME
 fi
 
-# replace user dir setup (symlink sadly does not work in this case)
-
-HOME_DIRS="Downloads Desktop Templates Public Documents Music Pictures Video"
-REPLACEMENT_DIRS="downloads dump/desktop dump/templates dump/public docs dump/music dump/pictures dump/videos"
-
-for i in $HOME_DIRS; do
-  if [ -d $HOME/$i ]; then
-    rmdir $HOME/$i
-  fi
-done
-
-for i in $REPLACEMENT_DIRS; do
-  if [ ! -d $HOME/$i ]; then
-    mkdir -p $HOME/$i
-  fi
-done
-
-rm -f $HOME/.config/user-dirs.dirs
-cp $HOME/dotfiles/sink/.config/user-dirs.dirs $HOME/.config/user-dirs.dirs
-
 # setup git config (if not already set)
-
 if [ ! -f $HOME/.git-credentials ]; then
   git config --global credential.helper store
 fi
@@ -90,16 +51,15 @@ if [[ ! "$GIT_CONF" == *"user.name"* ]]; then
 fi
 
 # ask to setup zsh shell
-
 if [[ ! "$SHELL" == *"/zsh"*   ]]; then
   read -p "Would you like to set zsh as the default shell? (y/n): " SWITCH
 
   if [[ "$SWITCH" == "y" ]]; then
     printf "Please run:"
     printf "sudo chsh -s \$(which zsh)"
-    printf "For the shell change to take effect, you will need to reboot the machine. I know, it's weird.\n"
+    printf "For the shell change to take effect, you will need to reboot the machine."
+    printf "If the shell change does not take effect on reboot, edit /etc/passwd\n"
   fi
 else
   printf "\nSetup complete.\n"
 fi
-
